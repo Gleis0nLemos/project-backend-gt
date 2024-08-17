8bb297b938829b59ff912e559a714de0ace50fe7
Gleison Lemos <gleison04lemos@gmail.com>
2024-08-17 14:01:56 -0300

    feat: create endpoint update user and auth with jwt token

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const { User } = require('../models');
 
 const getUserById = async (req, res) => {
@@ -23,6 +24,10 @@ const createUser = async (req, res) => {
   try {
     const { firstname, surname, email, password, confirmPassword } = req.body;
 
+    if (!req.body || typeof req.body !== 'object') {
+      return res.status(400).json({ message: 'Invalid request body' });
+    }
+
     // basic verification
     if (!firstname || !surname || !email || !password || !confirmPassword) {
       return res.status(400).json({ message: 'All fields are required' });
@@ -38,23 +43,96 @@ const createUser = async (req, res) => {
       return res.status(400).json({ message: 'Email is already in use' });
     }
 
-    // criptography password
-    const hashedPassword = await bcrypt.hash(password, 10);
-
     // create user
     const newUser = await User.create({
       firstname,
       surname,
       email,
-      password: hashedPassword,
+      password,
     });
+    // console.log('New user created:', newUser);
+    console.log('JWT_SECRET:', process.env.JWT_SECRET);
+
+    // generate token
+    const token = jwt.sign(
+      { id: newUser.id, email: newUser.email },
+      process.env.JWT_SECRET,
+      { expiresIn: '1h' }
+    );
 
     res.status(201).json({
       id: newUser.id,
       firstname: newUser.firstname,
       surname: newUser.surname,
-      email: newUser.email
+      email: newUser.email,
+      token, // return token
     });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+}
+
+const authenticateUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // verify if user exists
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      return res.status(401).json({ message: 'Email or password is incorrect' });
+    }
+    // console.log('Password provided:', password);
+    // console.log('Hashed password from DB:', user.password);
+    // verify password
+    const isPasswordValid = await bcrypt.compare(req.body.password, user.password);
+    //console.log('Password is valid:', isPasswordValid);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: 'Email or password is incorrect' });
+    }
+
+    // generate token
+    const token = jwt.sign(
+      { id: user.id, email: user.email },
+      process.env.JWT_SECRET,
+      { expiresIn: '1h' }
+    );
+
+    res.status(200).json({ token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
+const updateUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { firstname, surname, email } = req.body;
+
+    // verify if id in token is the same as id in params
+    if (req.user.id !== parseInt(id, 10)) {
+      return res.status(403).json({ message: 'You can only update your own profile' });
+    }
+
+    // basic validation
+    if (!firstname || !surname || !email) {
+      return res.status(400).json({ message: 'All fields are required' });
+    }
+
+    // check if user exists
+    const user = await User.findByPk(id);
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    // update user data
+    await User.update(
+      { firstname, surname, email },
+      { where: { id } }
+    );
+
+    res.status(204).send(); // No Content
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Internal server error' });
@@ -64,4 +142,6 @@ const createUser = async (req, res) => {
 module.exports = {
   getUserById,
   createUser,
+  authenticateUser,
+  updateUser,
 };
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { getUserById, createUser } = require('../controllers/userController');
+const { authenticateUser, getUserById, createUser, updateUser } = require('../controllers/userController');
+const authenticateToken = require('../middleware/authMiddleware');
 
+// route to authenticate user and generate token
+router.post('/auth/login', authenticateUser);
+
+// other routes
 router.get('/user/:id', getUserById);
 router.post('/user', createUser);
+router.put('/user/:id', authenticateToken, updateUser);
 
 module.exports = router;
